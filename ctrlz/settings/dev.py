4da9a13614a1c84978b1a85b6c92e1baba035926
Rees McDevitt <mcdevittrees@gmail.com>
2019-11-05 20:05:25 -0500

    inital commit

---
+++ ctrlz/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'nwy1eibx5_dw+ixwi-vh!u%a&l(w@_!8@=^ioys3x$b^abj&n2'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
